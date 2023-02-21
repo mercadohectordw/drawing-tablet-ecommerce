3820453fb42857a0103b798c41eab1a2d711b9a1
[mercadohectordw] <[mercadohectordw@gmail.com]>
2023-02-21 08:37:53 -0300

    creacion de componentes principales

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { ProductComponent } from './components/pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "profile", component: ProfileComponent},
+  {path: "product", component: ProductComponent},
+  {path: "", redirectTo: "home", pathMatch: 'full'},
+  {path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { ProdListComponent } from './components/pages/prod-list/prod-list.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { ProductComponent } from './components/pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProdListComponent,
+    ProfileComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
