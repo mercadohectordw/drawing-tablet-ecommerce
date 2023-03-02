7565fea52d8906f02ba651972e1eaa880b31aeb9
[mercadohectordw] <[mercadohectordw@gmail.com]>
2023-03-02 13:35:44 -0300

    middleware de verificacion de token

---
+++ backend/src/middleware/authorization.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const verifyTokenUsers = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader || authHeader === 'null'){
+    return res.status(403).send('User UnAuthorized');
+  }
+  if(!authHeader.toLowerCase().includes("bearer ")){
+    return res.status(400).send('Invalid Token');
+  }
+
+  let token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+    if(err){
+      return res.status(403).send('User UnAuthorized');
+    }
+
+    next();
+  });
+}
+
+module.exports = {
+  verifyToken
+};
