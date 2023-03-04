9057d70b50175b87096def11d7e2ca0897dc8919
[mercadohectordw] <[mercadohectordw@gmail.com]>
2023-03-04 15:30:31 -0300

    actualizacion carga de db

--- backend/database_data.sql
+++ backend/database_data.sql
@@ -17,3 +17,30 @@ CREATE TABLE `admin`(
     PRIMARY KEY (`user_id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
+
+CREATE TABLE `category`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(511),
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `product`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(1023),
+    `price` DECIMAL(10, 0) NOT NULL,
+    `main_image` VARCHAR(255),
+    `category_id` INT NOT NULL,
+    `inventory` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)    
+);
+
+CREATE TABLE `product_image`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `url` INT NOT NULL,
+    `product_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+);
