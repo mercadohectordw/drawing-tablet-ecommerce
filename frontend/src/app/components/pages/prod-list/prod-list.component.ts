abe9739ecd76e2e5ca4265cf1a55577b503e7662
[mercadohectordw] <[mercadohectordw@gmail.com]>
2023-03-14 14:29:17 -0300

    paginas de categorias y fix a footer

--- frontend/src/app/components/pages/prod-list/prod-list.component.ts
+++ frontend/src/app/components/pages/prod-list/prod-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Product } from 'src/app/models/Product';
 import { ProductService } from 'src/app/services/product.service';
@@ -12,19 +13,29 @@ export class ProdListComponent implements OnInit {
 
   categoryId?: number;
   productList!: Product[];
+  sortby = ["Price: Low to High", "Price: High to Low"];
+  sort: FormGroup;
+  path = "";
 
-  constructor(private productService: ProductService, private router:Router) { }
+  constructor(private formBuilder: FormBuilder, private productService: ProductService, private router:Router) {
+    this.sort = formBuilder.group({
+      by: ["", [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
     switch(window.location.pathname){
       case "/pen-tablets": 
         this.categoryId = 1;
+        this.path = "Pen Tablets";
         break;
       case "/pen-displays": 
         this.categoryId = 2;
+        this.path = "Pen Displays";
         break;
       case "/accessories": 
         this.categoryId = 3;
+        this.path = "Accessories";
         break;
     }
 
@@ -36,6 +47,17 @@ export class ProdListComponent implements OnInit {
         });
     } else {
       this.router.navigateByUrl("/page-not-found");
+    }
+  }
+
+  submitSort(): void{
+    if(this.sort.valid){
+      if(this.sort.get("by")?.value == "Price: Low to High"){
+        this.productList.sort((prodA: Product, prodB: Product) => prodA.price - prodB.price);
+      }
+      if(this.sort.get("by")?.value == "Price: High to Low"){
+        this.productList.sort((prodA: Product, prodB: Product) => prodA.price + prodB.price);
+      }
     }
   }
 }
